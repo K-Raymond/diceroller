b4ea54238c82169427d29dd42f747a54a8c12b97
Kurtis Raymond <kurtis1@excalibur.bc.ca>
2015-02-25 16:48:25 -0800

    Updating diceroller to be more flexable for future addtions

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # diceroller
+
+This is a basic dice roller for generating random dice.
+
+Usage is done by,
+./diceroller XdY+Z
+
+There are a small amount of options availble. These can be found by,
+
+./diceroller -h
--- diceroll.py
+++ diceroll.py
@@ -2,18 +2,57 @@
 
 import random
 from random import SystemRandom
+import argparse
 
-cont = "y"
-while cont.strip() == "y":
-    input = raw_input("Input X dice with Y sides (XdY): ").lower()
-    qnty = input[0:input.find("d")]
-    sides = input[input.find("d")+1:]
+# Parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("normal", help="Do a normal XdY+Z roll and find the sum",
+                    type=str)
+parser.add_argument("-a","--advantage", help="Roll with advantage mechanic",
+                    action="store_true")
+parser.add_argument("-r","--altrandom", help="Use python randomness instead of OS randomness",
+                    action="store_true")
+
+args = parser.parse_args()
+
+# Functions
+def howto():
+    print './diceroll.py XdY+C [args]'
+
+def singleroll(sides): # Roll one die with x sides
+    if args.altrandom:
+        x = random.randint(1,int(sides))
+    else:
+        x = SystemRandom().randint(1,int(sides))
+    return x
+
+def advantage(sides): # Advantage mechanic for dnd 5e
+    x = []
+    for i in range(2):
+        y = singleroll(sides)
+        x.append(y)
+    return max(x)
+
+def bulkroll(qnty,sides):
     x=0
     for i in range(int(qnty)):
-        A = SystemRandom().randint(1,int(sides))
-        x = x + A
-        print "Roll",i,"is",A
-    
-    print "Result of rolling",qnty,"dice, with",sides,"sides each, is:",x
+        y = singleroll(sides)
+        x = x + y
+    return x
 
-    cont = raw_input("Roll again? (y/n) ").lower()
+# Setting up dice input
+input = args.normal.lower()
+
+# Optional addition onto the roll
+addon = 0
+if input.find("+") != -1:
+    addon = int(input[input.find("+")+1:])
+    input = input[0:input.find("+")]
+
+# Separate input into basic parts
+qnty = input[0:input.find("d")]
+sides = input[input.find("d")+1:]
+
+# Basic dice roll
+result = bulkroll(qnty,sides)
+print result + addon
