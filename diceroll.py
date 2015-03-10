133a7558ad2f6d4d62044dc1921273e3762966d1
Kurtis Raymond <kurtis1@excalibur.bc.ca>
2015-03-09 17:31:57 -0700

    Added highest flag

--- diceroll.py
+++ diceroll.py
@@ -13,25 +13,35 @@ parser.add_argument("-a","--advantage", help="Roll with advantage mechanic",
                     action="store_true")
 parser.add_argument("-r","--altrandom", help="Use python randomness instead of OS randomness (Faster)",
                     action="store_true")
-parser.add_argument("-e","--experiment", type=int, help="Experimentally calculate the distribution of values from dice rolls")
+parser.add_argument("-e","--experiment", type=int, metavar='<x>',
+                    help="Experimentally calculate the distribution of values from <x> distinct dice rolls")
+parser.add_argument("-H","--highest", metavar='<x>', type=int, help="Take the highest <x> dice from a roll")
 
 args = parser.parse_args()
 
 # Functions
-def singleroll(sides): # Roll one die with x sides
+def rolldie(sides): # Roll one die with x sides
     if args.altrandom:
-        x = random.randint(1,int(sides))
+        x = random.randint(1,sides)
     else:
-        x = SystemRandom().randint(1,int(sides))
+        x = SystemRandom().randint(1,sides)
     return x
 
 # Do a roll and take the maximum numb of rolls qnty>=numb
 ## For this function, advantage is qnty=2 and numb=1
-def rollmaximum(sides,qnty = 1,numb = 1,takesum = True):
+def maxroll(sides,qnty = 1,numb = 1,takesum = True):
     x = []
-    for i in range(qnty):
-            y = singleroll(sides)
+    z = 0
+    if (qnty==1 and numb==1): # A normal roll
+        return rolldie(sides)
+    elif (qnty ==2 and numb==1): # May not be worthwhile to have this here
+        x1 = rolldie(sides)
+        x2 = rolldie(sides)
+        return max(x1,x2)
+    while z < qnty:
+            y = rolldie(sides)
             x.append(y)
+            z = z + 1
     x = sorted(x, reverse = True)
     if takesum:
         return sum(x[0:numb])
@@ -42,11 +52,11 @@ def advantage(sides): # Advantage mechanic for dnd 5e
     x = []
     if args.advantage:
             for i in range(2):
-                y = singleroll(sides)
+                y = rolldie(sides)
                 x.append(y)
                 output = max(x)
     else:
-        output = singleroll(sides)
+        output = rolldie(sides)
     return output
 
 def bulkroll(qnty,sides):
@@ -58,7 +68,7 @@ def bulkroll(qnty,sides):
         x=x+1
     return rollsum
 
-def makelist(qnty,sides):
+def makelist(qnty,sides): # generate a list of zeros, may change this to while-loop
     x = []
     for i in range(qnty*sides-qnty+1):
         x.append(0)
@@ -94,7 +104,10 @@ sides = int(input[input.find("d")+1:])
 ### End XdY+Z analysis
 
 if args.experiment is not None: # Experimental Array
-    rollarray(qnty,sides)     
+    rollarray(qnty,sides)
+elif args.highest is not None:
+    numb = args.highest
+    print maxroll(sides,qnty,numb) + addon
 else: # Normal dice roll
     result = bulkroll(qnty,sides)
     print result + addon
